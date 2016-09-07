15702201024ca8137069480f06b103a2caa8c4cf
Ernesto Rapetti <erapetti@yahoo.com>
2016-09-07 11:15:23 -0300

    Cambio de comillas en las consultas SQL. Descuento 2 días en las licencias por duelo
    cambio de autor

--- api/models/Certificados.js
+++ api/models/Certificados.js
@@ -26,48 +26,50 @@ module.exports = {
 		observaciones: 'string',
 	},
 	get: function(data,callback) {
-		return this.query('												\
-		SELECT certid,													\
-		       C.InasisLicFecha,											\
-		       A.cedula,												\
-		       A.PerNombreCompleto,											\
-		       C.InasisLicFchIni,											\
-		       C.InasisLicFchFin,											\
-		       case codlic												\
-				when "J" then "Junta Médica"									\
-				when "L" then "Lactancia"									\
-				when "M" then "Maternidad"									\
-				when "E" then "Enfermedad"									\
-				when "EM" then "Medio horario por enfermedad"							\
-				when "LM" then "Medio horario por lactancia"							\
-		       end tipoLicencia,											\
-		       C.cargo,													\
-		       C.horas,													\
-		       observaciones												\
-		 FROM Personal.certificaciones_anep C										\
-		 JOIN as400 A													\
-		   ON cast((A.cedula/10) as decimal)=C.cedula									\
-		 JOIN Personas.PERSONASDOCUMENTOS										\
-		   ON perdocid=cast(A.cedula as char(30)) AND paiscod="UY" AND doccod="CI"						\
-		WHERE codlic<>"N"												\
-		  AND C.InasisLicFchFin>=?											\
-		  AND C.InasisLicFchIni<=?											\
-		  AND A.dependid=?												\
-		  AND A.mes=?													\
-		  AND A.tipo<>1													\
-		  AND NOT EXISTS (												\
-			SELECT 1												\
-			FROM INASISLIC I											\
-                 	JOIN FUNCIONES_ASIGNADAS USING (FuncAsignadaId)								\
-                 	JOIN SILLAS USING (SillaId)										\
-			WHERE personalperid=perid										\
-			  AND C.InasisLicFchIni=I.InasisLicFchIni								\
-			  AND C.InasisLicFchFin=I.InasisLicFchFin								\
-		  	  AND SillaDependid=?											\
-		  )														\
-		GROUP BY certid,C.InasisLicFecha,A.cedula,C.InasisLicFchIni,C.InasisLicFchFin,codlic,C.cargo,C.horas,observaciones \
-		ORDER BY perdocid,C.InasisLicFchIni										\
-		',
+		return this.query(`
+		SELECT certid,
+		       C.InasisLicFecha,
+		       A.cedula,
+		       A.PerNombreCompleto,
+		       C.InasisLicFchIni,
+		       C.InasisLicFchFin,
+		       case codlic
+            when "J" then "Junta Médica"
+            when "L" then "Lactancia"
+            when "M" then "Maternidad"
+            when "E" then "Enfermedad"
+            when "EM" then "Medio horario por enfermedad"
+            when "LM" then "Medio horario por lactancia"
+		       end tipoLicencia,
+		       C.cargo,
+		       C.horas,
+		       observaciones
+		 FROM Personal.certificaciones_anep C
+		 JOIN as400 A
+		   ON cast((A.cedula/10) as decimal)=C.cedula
+		 JOIN Personas.PERSONASDOCUMENTOS
+		   ON perdocid=cast(A.cedula as char(30)) AND paiscod="UY" AND doccod="CI"
+		WHERE codlic<>"N"
+		  AND C.InasisLicFchFin>=?
+		  AND C.InasisLicFchIni<=?
+		  AND A.dependid=?
+		  AND A.mes=?
+		  AND A.tipo<>1
+		  AND NOT EXISTS (
+			SELECT 1
+			FROM INASISLIC I
+                 	JOIN FUNCIONES_ASIGNADAS USING (FuncAsignadaId)
+                 	JOIN SILLAS USING (SillaId)
+			WHERE personalperid=perid
+			  AND C.InasisLicFchIni=I.InasisLicFchIni
+			  AND C.InasisLicFchFin=I.InasisLicFchFin
+		  	  AND SillaDependid=?
+		  )
+		GROUP BY certid,C.InasisLicFecha,A.cedula,
+             C.InasisLicFchIni,C.InasisLicFchFin,
+             codlic,C.cargo,C.horas,observaciones
+		ORDER BY perdocid,C.InasisLicFchIni
+		`,
 		[data.Anio+'-'+data.Mes+'-01', data.Anio+'-'+data.Mes+'-31', data.DependId, data.Mes, data.DependId],
 		callback);
 	},
--- api/models/Inasistencias.js
+++ api/models/Inasistencias.js
@@ -19,28 +19,27 @@ module.exports = {
 		inasistencias: 'integer',
 	},
 	get: function(data,callback) {
-		return Inasistencias.query('												\
-																	\
-SELECT perdocid, InasisLicTipo, SUM(ifnull(InasisLicId_Dias,1)) inasistencias								\
-FROM (																	\
-	SELECT *															\
-	FROM Personal.INASISLIC														\
-	JOIN FUNCIONES_ASIGNADAS USING (FuncAsignadaId)											\
-	JOIN SILLAS USING (SillaId)													\
-	WHERE SillaDependId=?														\
-	  AND InasisLicFchFin>=?													\
-	  AND InasisLicFchIni<=?													\
-	GROUP BY PERSONALPERID,INASISLICID												\
-) I																	\
-JOIN INASCAUSALES USING (InasCausId)													\
-JOIN Personas.PERSONASDOCUMENTOS ON personalperid=perid and paiscod="UY" and doccod="CI"						\
-LEFT JOIN INASISLIC_LICENCIA_DIAS ID on ID.InasisLicId=I.InasisLicId and ID.PersonalPerId=I.PersonalPerId and ID.InasisLicId_Mes=?	\
-WHERE inascauspres=true															\
-GROUP BY perdocid,InasisLicTipo,InasisLicFchIni,InasisLicFchFin										\
-																	\
-		',
-
-		[data.DependId, data.Anio+'-'+data.Mes+'-01', data.Anio+'-'+data.Mes+'-31', data.Mes],
-		callback);
+		return Inasistencias.query(`
+    SELECT perdocid,
+      InasisLicTipo,
+      IF(inascauspres=3, SUM(ifnull(InasisLicId_Dias,1)-2), SUM(ifnull(InasisLicId_Dias,1))) inasistencias
+    FROM (
+    	SELECT *
+    	FROM Personal.INASISLIC
+    	JOIN FUNCIONES_ASIGNADAS USING (FuncAsignadaId)
+    	JOIN SILLAS USING (SillaId)
+    	WHERE SillaDependId=?
+    	  AND InasisLicFchFin>=?
+    	  AND InasisLicFchIni<=?
+    	GROUP BY PERSONALPERID,INASISLICID
+    ) I
+    JOIN INASCAUSALES USING (InasCausId)
+    JOIN Personas.PERSONASDOCUMENTOS ON personalperid=perid and paiscod="UY" and doccod="CI"
+    LEFT JOIN INASISLIC_LICENCIA_DIAS ID on ID.InasisLicId=I.InasisLicId and ID.PersonalPerId=I.PersonalPerId and ID.InasisLicId_Mes=?
+    WHERE inascauspres<>0
+    GROUP BY perdocid,InasisLicTipo,InasisLicFchIni,InasisLicFchFin`
+      ,
+		  [data.DependId, data.Anio+'-'+data.Mes+'-01', data.Anio+'-'+data.Mes+'-31', data.Mes],
+		  callback);
 	},
 };
