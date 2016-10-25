026fa94df48850fab2bd2d69a66e6e7e24016afd
Ernesto Rapetti <erapetti@yahoo.com>
2016-10-25 16:05:22 -0300

    Ajuste para no dar error cuando la sesión empieza con espacio

--- api/services/wsPortal.js
+++ api/services/wsPortal.js
@@ -6,25 +6,33 @@ module.exports = {
 		// recibe string con el número de sesión
 		// llama a callback con parámetros err y {Sesionesid,Userid,Dependid,Lugarid}
 
-		if (typeof sesionId === 'undefined' || ! sesionId.match(/^ *([a-zA-Z\d]+) *$/)) {
+		// saco espacios y/o signos de más que pueden venir al inicio o al final de sesionId
+		sesionId = sesionId.replace(/^[+ ]*/, '').replace(/[+ ]*$/, '');
+
+		if (typeof sesionId === 'undefined' || ! sesionId.match(/^([a-zA-Z\d]+)$/)) {
+			sails.log("Sesión no válida: ",sesionId);
 			return callback(new Error("Sesión no válida. Reinicie su conexión con el portal de servicios"),undefined);
 		}
 
 		var soap = require('soap');
 		soap.createClient('api/services/aws_dame_datos_de_sesion.wsdl', function(err, client) {
 			if (err) {
+				sails.log("aws_dame_datos_de_sesion: ",err);
 				return callback(err,undefined);
 			}
 			client.Execute({Sesionesid:sesionId}, function(err, result) {
 
 				if (!result.Userid) {
+					sails.log("Userid es vacío");
 					return callback(new Error("Sesión no válida. Reinicie su conexión con el portal de servicios"),undefined);
 				}
 				wsPortal.leoPermiso({Sesionesid:sesionId,Programa:'presentismo',Modo:'DSP'}, function(err, permiso) {
 					if (err) {
+						sails.log("leoPermiso: ",err);
 						return callback(err, undefined);
 					}
 					if (permiso.Autorizado !== "S") {
+						sails.log("leoPermiso: No tiene los permisos requeridos");
 						return callback(new Error("No tiene los privilegios requeridos para acceder a esta página"), undefined);
 					}
 					return callback(undefined,result);
