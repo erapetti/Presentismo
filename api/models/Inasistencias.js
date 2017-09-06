f1749455d23c79e5128e112cd8f3262c8383118f
Ernesto Rapetti <erapetti@yahoo.com>
2017-09-06 13:14:32 -0300

    Ajustes relacionados a las enmiendas

--- api/models/Inasistencias.js
+++ api/models/Inasistencias.js
@@ -35,11 +35,11 @@ module.exports = {
     ) I
     JOIN INASCAUSALES USING (InasCausId)
     JOIN Personas.PERSONASDOCUMENTOS ON personalperid=perid and paiscod="UY" and doccod="CI"
-    LEFT JOIN INASISLIC_LICENCIA_DIAS ID on ID.InasisLicId=I.InasisLicId and ID.PersonalPerId=I.PersonalPerId and ID.InasisLicId_Mes=?
+    LEFT JOIN INASISLIC_LICENCIA_DIAS ID on ID.InasisLicId=I.InasisLicId and ID.PersonalPerId=I.PersonalPerId and ID.InasisLicId_Mes=? and ID.InasisLicId_Anio=?
     WHERE inascauspres<>0
     GROUP BY perdocid,InasisLicTipo,InasisLicFchIni,InasisLicFchFin`
       ,
-		  [data.DependId, data.Anio+'-'+data.Mes+'-01', data.Anio+'-'+data.Mes+'-31', data.Mes],
+		  [data.DependId, data.Anio+'-'+data.Mes+'-01', data.Anio+'-'+data.Mes+'-31', data.Mes, data.Anio],
 		  callback);
 	},
   get_multas: function(data,callback) {
@@ -49,13 +49,13 @@ module.exports = {
       InasCausTipo,
       InasCausId,
       CicloPago,
-      sum(horas) horas,
-      sum(ifnull(InasisLicId_Dias, dias)) dias
+      sum(horas*if(InasisLicTipoMov='E',-1,1)) horas,
+      sum(ifnull(InasisLicId_Dias, dias)*if(InasisLicTipoMov='E',-1,1)) dias
     FROM (
-      SELECT PERSONALPERID,InasisLicTipo,InasisLicFchIni,InasisLicFchFin,InasisLicId,InasCausId,
+      SELECT PERSONALPERID,InasisLicTipo,InasisLicFchIni,InasisLicFchFin,InasisLicId,InasCausId,InasisLicTipoMov,
              if(InasisLicCicloPago is not null,InasisLicCicloPago,RelLabCicloPago) CicloPago,
-             sum(if(InasisLicDiaHora='H',InasisLicCant*if(InasisLicTipoMov='E',-1,1),0)) horas,
-             sum(if(InasisLicDiaHora='D',InasisLicCant*if(InasisLicTipoMov='E',-1,1),0)) dias
+             sum(if(InasisLicDiaHora='H',InasisLicCant,0)) horas,
+             sum(if(InasisLicDiaHora='D',InasisLicCant,0)) dias
       FROM Personal.INASISLIC
       JOIN FUNCIONES_ASIGNADAS USING (FuncAsignadaId)
       JOIN SILLAS USING (SillaId)
@@ -64,15 +64,15 @@ module.exports = {
       WHERE SillaDependId=?
         AND InasisLicFchFin>=?
         AND InasisLicFchIni<=?
-      GROUP BY 1,2,3,4,5,6,7
+      GROUP BY 1,2,3,4,5,6,7,8
     ) I
     JOIN INASCAUSALES USING (InasCausId)
     JOIN Personas.PERSONASDOCUMENTOS ON personalperid=perid and paiscod="UY" and doccod="CI"
-    LEFT JOIN INASISLIC_LICENCIA_DIAS ID on ID.InasisLicId=I.InasisLicId and ID.PersonalPerId=I.PersonalPerId and ID.InasisLicId_Mes=?
+    LEFT JOIN INASISLIC_LICENCIA_DIAS ID on ID.InasisLicId=I.InasisLicId and ID.PersonalPerId=I.PersonalPerId and ID.InasisLicId_Mes=? and ID.InasisLicId_Anio=?
     WHERE InasCausDescuento<>0
-    GROUP BY perdocid,InasisLicTipo,InasCausTipo`
+    GROUP BY perdocid,InasisLicTipo,InasCausTipo,InasCausId,CicloPago`
       ,
-      [data.DependId, data.Anio+'-'+data.Mes+'-01', data.Anio+'-'+data.Mes+'-31', data.Mes ],
+      [data.DependId, data.Anio+'-'+data.Mes+'-01', data.Anio+'-'+data.Mes+'-31', data.Mes, data.Anio ],
       callback);
   },
 };
