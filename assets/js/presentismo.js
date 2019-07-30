81cb44ef07a3a4216bd2f0310b50f267f1d212e4
Ernesto Rapetti <erapetti@yahoo.com>
2019-07-30 20:25:58 -0300

    Agrego filtro en la página de pendientes para ocultar los que no tienen error

--- assets/js/presentismo.js
+++ assets/js/presentismo.js
@@ -1,3 +1,30 @@
+$('a#soloErrores').click(function(e) {
+  e.stopImmediatePropagation();
+  if ($(this).parent().hasClass("active")) {
+    $(this).parent().removeClass("active");
+    $('div#pendientes table.table tr:has(td:last-child span.text-success)').show();
+  } else {
+    $(this).parent().addClass("active");
+    $('div#pendientes table.table tr:has(td:last-child span.text-success)').hide();
+  }
+  showHideLabels();
+});
+
+function showHideLabels() {
+  var last = '';
+  $('div#pendientes table.table tr td:first-child').each(function(i,elem) {
+    if ($(elem).parent().is(':visible')) {
+      if ($(elem).text() == last) {
+        $(elem).addClass("invisible");
+      } else {
+        $(elem).removeClass("invisible");
+        last = $(elem).text();
+      }
+    }
+  });
+}
+showHideLabels();
+
 var $root = $('html, body');
 $('a').click(function() {
     var href = $.attr(this, 'href');
